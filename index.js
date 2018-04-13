b434c88a058baa791a86d3d9e685f2715d1f1495
Joe Tagliaferro <jtag05@users.noreply.github.com>
2018-04-13 17:49:33 -0400

    Working user create and pw check

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-const mongoose = require( 'mongoose' )
-const SinopiaUser = require( './lib/sinopiaUser.js' )
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+const SinopiaUser = require('./lib/sinopiaUser.js');
 
-function SinopiaMongodb( config, sinopia ) {
-
-    console.log( 'sinopia-mongodb plugin loaded.' )
+function SinopiaMongodb(config, sinopia) {
+    console.log('sinopia-mongodb plugin loaded.');
 
     /** Connection to mongodb database **/
-    if ( !config['host'] )
-        throw new Error( 'Need a valid host in config file.' );
+    if (!config['host']) {
+        throw new Error('Need a valid host in config file.');
+    }
 
-    var mongodb_uri = 'mongodb://' + config['host'] + ( config['port'] ? ':' + config['port'] : '') + '/' + (config['database'] ? config['database'] : '')
-    mongoose.connect( mongodb_uri, function ( err ) {
-        console.log( 'Connected to ' + mongodb_uri );
-        if ( err )
-            throw err
-    } )
+    const mongodb_uri = `mongodb://${config['host']}${config['port'] ? `:${config['port']}` : ''}/${config['database'] ? config['database'] : ''}`;
+    mongoose.connect(mongodb_uri, function (err) {
+        if (err) { throw err; }
+    });
 
     /**
      * Always return a cb(null, true) to allow to add an user
@@ -23,35 +22,75 @@ function SinopiaMongodb( config, sinopia ) {
      * @param callback
      * @returns {*}
      */
-    this.adduser = function ( user, password, callback ) {
-        return callback( null, true )
+    this.adduser = function (user, password, callback) {
+        if (config.registration_enabled) {
+            return SinopiaUser.findOne({ username: user }, function (err, doc) {
+                if (err) {
+                    return callback(err, null);
+                }
+                if (doc) {
+                    return callback(null, false);
+                }
+
+                bcrypt.genSalt(10, function (err, salt) {
+                    if (err) {
+                        return callback(err, null);
+                    }
+
+                    bcrypt.hash(password, salt, null, function (err, hash) {
+                        if (err) {
+                            return callback(err, null);
+                        }
+
+                        SinopiaUser.create({
+                            username: user,
+                            token: hash,
+                            type: 'user',
+                            enabled: true
+                        }, function (err, doc) {
+                            return callback(err, doc);
+                        });
+                    });
+                });
+            });
+        }
+
+        return callback(null, true);
     };
 
-    this.authenticate = function ( user, password, callback ) {
-
+    this.authenticate = function (user, password, callback) {
         // Find the
-        SinopiaUser.findOne( {shared_id: user, token: password}, function ( err, doc ) {
 
-            if ( err )
-                return callback( err, null )
+        SinopiaUser.findOne({ username: user }, function (err, doc) {
+            if (err) { return callback(err, null); }
 
-            if ( !doc )
-                return callback( null, false )
+            if (!doc) { return callback(null, false); }
 
-            doc.last_download = new Date()
-            doc.download += 1
-            doc.save( function ( err ) {
-                return callback( err, [user] )
-            } )
+            let pass = Promise.resolve();
+            if (password !== doc.token) {
+                pass = new Promise(function (resolve, reject) {
+                    bcrypt.compare(password, doc.token, function (err, isMatch) {
+                        if (err) {
+                            return reject(err);
+                        }
+                        return resolve(isMatch);
+                    });
+                });
+            }
 
-        } )
-
-    }
-
+            pass.then(function () {
+                doc.last_download = new Date();
+                doc.download += 1;
+                doc.save(function (err) {
+                    return callback(err, [ user ]);
+                });
+            }).catch(function (err) {
+                return callback(err, null);
+            });
+        });
+    };
 }
 
-module.exports = function ( config, stuff ) {
-
-    return new SinopiaMongodb( config, stuff )
-
+module.exports = function (config, stuff) {
+    return new SinopiaMongodb(config, stuff);
 };
